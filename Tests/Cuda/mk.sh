51e22c9c0fbf2b23f814f17c3aac300ede763c56
Borge Arntsen <borge.arntsen@gmail.com>
2023-06-08 14:56:13 +0200

    Debug changes

--- Tests/Cuda/mk.sh
+++ Tests/Cuda/mk.sh
@@ -2,17 +2,17 @@
 
 cp ../../Ac2d/libac2dcuda.o .
 cp ../../Ac2d/*.i .
-cp ../../Ac2d/ac2dmod.e .
+#cp ../../Ac2d/ac2dmod.e .
 
 ecc ac2dmod.e
 elc -o ac2dmod ac2dmod.o libac2dcuda.o 
 
 #Create wavelet
-nt=1501 #No of samples
+nt=1001 #No of samples
 ricker -nt $nt -f0 30.0 -t0 0.100 -dt 0.0005 src.bin 
 
-n1=251
-n2=251
+n1=1001
+n2=1001
 #Create vp
 spike -n1 $n1 -n2 $n2 -val 2500.0 vp.bin
 
@@ -23,9 +23,9 @@ spike -n1 $n1 -n2 $n2 -val 1000.0 rho.bin
 spike -n1 $n1 -n2 $n2 -val 100000.0 q.bin
 
 #Run modelling
-./ac2dmod 
+#./ac2dmod 
 
-../../Scripts/snp.sh
+#../../Scripts/snp.sh
 
 
 
